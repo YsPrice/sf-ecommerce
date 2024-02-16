a3268a6a485e9dfbb5967834d45d1c123582b8ff
ashavan33 <105936390+ashavan33@users.noreply.github.com>
2024-02-16 15:53:15 -0500

    pg

--- db/migrate/20231222001550_remove_encrypted_pass_and_username_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveEncryptedPassAndUsernameFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :encrypted_pass
-    remove_column :users, :username
-  end
-end
