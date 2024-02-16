533c9b6e9dc221333859ccb0d37bfedc1199d686
ashavan33 <105936390+ashavan33@users.noreply.github.com>
2024-02-16 16:20:20 -0500

    pg

--- db/migrate/20240119142724_add_product_id_to_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToLineItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :line_items, :cart_id, :integer, null: false
-  end
-end
--- db/migrate/20240131165908_add_defaults_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultsToOrders < ActiveRecord::Migration[7.0]
-  def change
-    change_column :orders, :order_number, :string, default: "default_order_number"
-    change_column :orders, :total_items, :integer, default: 0
-  end
-end
